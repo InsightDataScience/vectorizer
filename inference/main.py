6d1325f204794542f68d2f1c0f0a9c21577bd6ac
rexlintc <tc.lin@berkeley.edu>
2019-01-24 14:17:01 -0800

    rename clean to cleaner

--- inference/main.py
+++ inference/main.py
@@ -6,12 +6,13 @@ import logging
 import numpy as np
 import pandas as pd
 
-import clean
+import cleaner
 # import preprocess
 
 DATA_PATH = 'data/raw/testdata.text_only.csv'
 
 def load_data(path):
+	# current implementation assumes data input as single column text only csv
 	dataset = pd.read_csv(path, index_col=0)
 	dataset.columns = ['text']
 	return dataset
@@ -27,9 +28,9 @@ def main(clean_data, preprocess_data):
 		dataset = load_data(DATA_PATH)
 		print('before cleaning \n')
 		print(dataset.head(2))
-		dataset = clean.remove_URL(dataset, 'text')
-		dataset = clean.remove_special_characters(dataset, 'text')
-		dataset = clean.lowercase(dataset, 'text')
+		dataset = cleaner.remove_URL(dataset, 'text')
+		dataset = cleaner.remove_special_characters(dataset, 'text')
+		dataset = cleaner.lowercase(dataset, 'text')
 		print('after cleaning \n')
 		print(dataset.head(2))
 
