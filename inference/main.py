91828d0d0730a4a5086b7aebb8b9995d0ce6e213
rexlintc <tc.lin@berkeley.edu>
2019-01-24 07:05:11 -0800

    fix column renaming to allow for text only input format

--- inference/main.py
+++ inference/main.py
@@ -9,10 +9,10 @@ import pandas as pd
 import clean
 # import preprocess
 
-DATA_PATH = 'data/raw/testdata.manual.2009.06.14.csv'
+DATA_PATH = 'data/raw/testdata.text_only.csv'
 
 def load_data(path):
-	dataset = pd.read_csv(path, header=None)
+	dataset = pd.read_csv(path, index_col=0)
 	dataset.columns = ['text']
 	return dataset
 
@@ -20,9 +20,10 @@ def load_data(path):
 @click.option("--clean_data", is_flag=True)
 @click.option('--preprocess_data', is_flag=True)
 def main(clean_data, preprocess_data):
-	logging.getLogger().setLevel(logging.INFO)
-	# logging.INFO('Cleaning input data')
+	# logging.getLogger().setLevel(logging.INFO) # TODO: setup logging
+
 	if clean_data:
+		# logging.INFO('Cleaning input data')
 		dataset = load_data(DATA_PATH)
 		print('before cleaning \n')
 		print(dataset.head(2))
@@ -30,7 +31,7 @@ def main(clean_data, preprocess_data):
 		dataset = clean.remove_special_characters(dataset, 'text')
 		dataset = clean.lowercase(dataset, 'text')
 		print('after cleaning \n')
-		print(dataset.head(2))		
+		print(dataset.head(2))
 
 	return
 
--- inference/preprocess.py
+++ inference/preprocess.py
@@ -1,20 +1,6 @@
-def main():
-	# Initialize logger
-	LOG_FILENAME = 'case_automation.log'
-	logging.basicConfig(filename=LOG_FILENAME,
-                            filemode='a',
-                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                            datefmt='%H:%M:%S',
-                            level=logging.DEBUG)
-
-	pass
-
 from nltk.tokenize import RegexpTokenizer
 
-def tokenize(df, text_field):
+def tokenize(df):
 	tokenizer = RegexpTokenizer(r'\w+')
 	df["tokens"] = df["text"].apply(tokenizer.tokenize)
 	return df
-
-if __name__ == '__main__':
-    main()
