98e0cd10799db43944d3e96e61e7e489fbcedd6e
Pujaa Rajan <pujaarajan@Pujaas-MacBook-Pro.local>
2019-01-25 01:36:32 -0800

    working unigrams and unigram count

--- src/ngram.py
+++ src/ngram.py
@@ -12,8 +12,10 @@ class Ngram:
         self.log.info('Starting to create ngram model inputs')
         self.unigrams = self.ngram_generator(preprocessed_dataframe, 1)
         self.word_in_document_count = self.word_in_document_counter(preprocessed_dataframe)
-        self.word_count = self.word_counter(Counter(), preprocessed_dataframe)
-        print(self.word_count)
+        self.unigram_count = self.ngram_counter(Counter(), self.unigrams)
+
+        # self.word_count = self.word_counter(Counter(), preprocessed_dataframe) # same as unigram_count, not used
+
 
     def ngram_generator(self, preprocessed_dataframe, n):
         self.log.info(f'Creating {n} grams')
@@ -29,36 +31,17 @@ class Ngram:
         # NLTK version below. This is creating a word count per document.
         return preprocessed_dataframe.apply(lambda row: nltk.FreqDist(row))
 
-    def word_counter(self, counter, preprocessed_dataframe):
-        # Because the data is stored as a list of lists of word, I need to iterate through them to be able to count.
-        for row in preprocessed_dataframe:
-            counter.update(row)
+    def ngram_counter(self, counter, ngram):
+        for doc in ngram:
+            for word in doc:
+                counter.update(word)
         return counter
 
-    def count_unigram(self, preprocessed_dataframe, bi_dict, vocab_dict, nlp):
-
-        token = []
-        # total no. of words in the corpus
-        word_len = 0
-
-        # open the corpus file and read it line by line
-        # read in better
-        spacy_tokenized_emails = []
-        corpus_size = 0
-        with open(file_path, 'r') as file:
-            for line in file:
-                line = line.strip()  # checking to see if line is blank
-                if not line:  # line is blank
-                    continue
-                line_tokens = []
-                doc = nlp(line)
-                for token in doc:
-                    line_tokens.append(token.text)
-                spacy_tokenized_emails.append(line_tokens)
-                # ? missing relationships of words across lines
-                corpus_size = corpus_size + len(line_tokens)
-
-                add_words_to_vocab(line_tokens, vocab_dict)
-
-                bi_dict.update(ngrams(line_tokens, 2))
-        return vocab_dict
+    # I don't think I need this anymore. It was originally used to create word count, but I greated ngram_counter
+    # to be able to count not only individual words but also bigrams etc. Saving this just in case it fails.
+    #
+    # def word_counter(self, counter, preprocessed_dataframe):
+    #     # Because the data is stored as a list of lists of word, I need to iterate through them to be able to count.
+    #     for row in preprocessed_dataframe:
+    #         counter.update(row)
+    #     return counter
