07b7ccc8d36ddef5db064ca99aba3680c727dad6
Pujaa Rajan <pujaarajan@Pujaas-MacBook-Pro.local>
2019-01-25 02:30:29 -0800

    Deleted old ngram generation and count code that will exist in the prev commit if need be (reminder to self)

--- src/ngram.py
+++ src/ngram.py
@@ -19,15 +19,6 @@ class Ngram:
         self.bigrams, self.bigram_count = self.ngram_generator_and_counter(preprocessed_dataframe, 2, bigram_counter)
         print(self.bigram_count)
 
-        # OLD CODE THAT I CHANGED ABOVE BUT SAVING THIS IN CASE IT FAILS
-
-        # self.word_count = self.word_counter(Counter(), preprocessed_dataframe) # same as unigram_count, not used
-
-        # self.unigrams = self.ngram_generator(preprocessed_dataframe, 1) # currently a generator object
-        # self.unigram_count = self.ngram_counter(Counter(), self.unigrams, 1)
-
-        # self.bigrams = self.ngram_generator(preprocessed_dataframe, 2) # list of bigrams
-        # self.bigram_count = self.ngram_counter(Counter(), self.bigrams, 2)
 
     def word_in_document_counter(self, preprocessed_dataframe):
 
@@ -70,22 +61,3 @@ class Ngram:
                 counter.update(x)
         return list_of_ngrams, counter
 
-    # I don't think I need this anymore. It was originally used to create word count, but I greated ngram_counter
-    # to be able to count not only individual words but also bigrams etc. Saving this just in case it fails.
-    #
-    # def word_counter(self, counter, preprocessed_dataframe):
-    #     # Because the data is stored as a list of lists of word, I need to iterate through them to be able to count.
-    #     for row in preprocessed_dataframe:
-    #         counter.update(row)
-    #     return counter
-    #
-    # def ngram_generator(self, preprocessed_dataframe, n):
-    #     self.log.info(f'Creating {n} grams')
-    #     if n>1:
-    #         # This if statement exists if we are calculating bigrams or above because then the nltk ngram function
-    #         # doesn't work properly unless we are iterating through the documents and calculating ngrams
-    #         list_of_ngrams = []
-    #         for row in preprocessed_dataframe:
-    #             list_of_ngrams.append(list(ngrams(row, n)))
-    #         return list_of_ngrams
-    #     return ngrams(preprocessed_dataframe,n)
