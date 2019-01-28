3d66c411f12cad198f8924b4ff0200da90c45b98
rexlintc <tc.lin@berkeley.edu>
2019-01-27 17:55:32 -0800

    tested keras pipeline with glove and implemented keras tokenization in modular pipeline

--- inference/preprocesser.py
+++ inference/preprocesser.py
@@ -1,6 +1,25 @@
 from nltk.tokenize import RegexpTokenizer
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 
 def tokenize(df):
 	tokenizer = RegexpTokenizer(r'\w+')
 	df["tokens"] = df["text"].apply(tokenizer.tokenize)
 	return df
+
+def keras_tokenizer(df):
+	t = Tokenizer()
+	t.fit_on_texts(df)
+
+	vocab_size = len(t.word_index) + 1
+	encoded_docs = t.texts_to_sequences(tweet_doc)
+
+	max_length = get_max_length(encoded_docs)
+	padded_docs = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
+	return padded_docs
+
+def get_max_length(docs)
+	sentence_lengths = []
+	for doc in docs:
+	    sentence_lengths.append(len(doc))
+	return max(sentence_lengths)
