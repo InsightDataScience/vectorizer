1e3f5c0a3bb5a8a3f28e8f325c32979cde7bd199
rexlintc <tc.lin@berkeley.edu>
2019-01-29 12:43:14 -0800

    flask hello world with curl

--- vectorizer/__init__.py
+++ vectorizer/__init__.py
@@ -1,3 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 from vectorizer import views
+
+# from flask_restful import Resource, Api
+#
+# app = Flask(__name__)
+# api = Api(app)
+#
+# class HelloWorld(Resource):
+#     def get(self):
+#         return {'hello': 'world'}
+#
+# api.add_resource(HelloWorld, '/')
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
--- vectorizer/views.py
+++ vectorizer/views.py
@@ -1,6 +1,19 @@
 from vectorizer import app
 
-@app.route('/')
-@app.route('/index')
-def index():
-  return "Hello, World!"
+from flask_restful import Resource, Api
+
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world this is my first test'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#   return "Hello, World!"
