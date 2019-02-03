03339ea80d9cf41a4ef12cb24a503b2e363cb8fc
rexlintc <tc.lin@berkeley.edu>
2019-02-03 10:20:48 -0800

    added checklist markdown and removed old folder

--- vectorizer_api/vectorizer/clean.py
+++ vectorizer_api/vectorizer/clean.py
@@ -1,18 +1,27 @@
 import pandas as pd
 import numpy as np
 
-def remove_URL(text):
-	text = text.replace(r"http\S+", "")
-	text = text.replace(r"http", "")
-	return text
+def clean_str(string):
+	# 2/2/19 Adapted from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+	"""
+	string cleaning
+	"""
+	string = re.sub(r"[^A-Za-z0-9]", " ", string)
+	string = re.sub(r"\s{2,}", " ", string) # removes consecutive white spaces
+	return string.strip().lower()
 
-def remove_special_characters(text):
-	# text = text.replace(r"@\S+", "")
-	text = text.replace(r"[^A-Za-z0-9 ]+", "")
-	# text = text.replace(r"[^A-Za-z0-9(),!?@\'\`\"\_\n]", " ")
-	# text = text.replace(r"@", "at")
-	return text
+def remove_URL(string):
+	string = string.replace(r"http\S+", "")
+	string = string.replace(r"http", "")
+	return string
 
-def lowercase(text):
-	text = text.lower()
-	return text
+def remove_special_characters(string):
+	# string = string.replace(r"@\S+", "")
+	string = string.replace(r"[^A-Za-z0-9 ]+", "")
+	# string = string.replace(r"[^A-Za-z0-9(),!?@\'\`\"\_\n]", " ")
+	# string = string.replace(r"@", "at")
+	return string
+
+def lowercase(string):
+	string = string.lower()
+	return string
