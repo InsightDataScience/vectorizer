7e381688d1af0ccd0078fadbcc360697e9d04305
Pujaa Rajan <pujaarajan@Pujaas-MacBook-Pro.local>
2019-02-06 12:27:37 -0800

    fixed logging

--- nlp_fib/src/ngram_train.py
+++ nlp_fib/src/ngram_train.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 class NgramTrain:
     def __init__(self, preprocessed_dataframe, input_file_path):
-        self.log = logging.getLogger('Enron_email_analysis.ngram')
+        self.log = logging.getLogger('Enron_email_analysis.ngram_test')
         self.log.info('Starting to create ngram model inputs.')
 
         # Do we need this? Keeping for now, but delete in future if not.
@@ -18,7 +18,6 @@ class NgramTrain:
         # and returning the value too. It has been initialized so I think you can update and access it directly.
         unigram_counter = Counter()
         self.unigrams, self.unigram_count = self.ngram_generator_and_counter(preprocessed_dataframe, 1, unigram_counter)
-        print(self.unigram_count)
         self.log.info(f'Number of words in unigram count: {len(list(self.unigram_count))}')
 
         bigram_counter = Counter()
@@ -73,16 +72,11 @@ class NgramTrain:
 
         logging.info(f'Calculating ngram probabilities in the {direction} direction')
         unique_word_count = len(unigram_count)
-        print(f'unigram count{unigram_count}')
-        print(f'unique word count{unique_word_count}')
         # create a dictionary of probable words with their probabilities for bigram probabilites
         for ngram_token in ngram_count:
-            print(f'for ngram token in ngram count -> ngram token is {ngram_token}')
             # unigram for key
             if direction=='forward':
-                print(f'in direction is forward -> confirming direction is {direction}')
                 unigram_token = ngram_token[0]
-                print(f'in direction is forward -> unigram token is {unigram_token}')
             elif direction=='backward':
                 unigram_token = ngram_token[-1]  # Start with the second or last word first and count backwards
             else:
@@ -92,13 +86,6 @@ class NgramTrain:
             # find the probability and add 1 smoothing has been used
             probability = (ngram_count[ngram_token] + 1) / (unigram_count[unigram_token] + unique_word_count)
             # HELP ? FOR BACKWARDS I AM ASSUMING THE NUMERATOR IS THE SAME AS FORWARDS PROBABILITY
-            print(f'probability for ngram token (which is {ngram_token} is {probability}')
-            print(f'unigram count of unigram token (which is {unigram_token}) is {unigram_count[unigram_token]}')
-            print(f'unique word count is {unique_word_count}')
-            print(f'numerator is {(ngram_count[ngram_token] + 1)}')
-            print(f'denominator is {(unigram_count[unigram_token] + unique_word_count)}')
-
-            print('START DEBUGGING HERE NOW THIS PART IS WRONG')
 
             if direction == 'forward':
                 last_ngram_token = ngram_token[-1]  # Take the second word
@@ -107,17 +94,11 @@ class NgramTrain:
 
             # bi_prob_dict is a dict of list and if the unigram sentence is not present in the Dictionary then add it
             if unigram_token not in ngram_probability: # Ngram probability is a counter
-                print(f'in unigram_token is not in ngram_probability')
-                print(f'specifically {unigram_token} is not in {ngram_probability}')
                 ngram_probability[unigram_token] = [] # create an empty list as the value after the first word
                 ngram_probability[unigram_token].append([probability, last_ngram_token])
             # the unigram sentence is present but the probable word is missing,then add it
             else:
-                print(f'I THINK THIS IS THE ISSUE WHAT IS THE VALUE OF LAST NGRAM TOKEN HERE: {last_ngram_token}')
                 ngram_probability[unigram_token].append([probability, last_ngram_token])
-            print(ngram_probability)
-        print(ngram_probability)
-
 
 # I think the ngram probability counter has been fixed now. Next step is to rerun training and try testing.
 # Do this to check if function that reads in the probabilities works correctly
