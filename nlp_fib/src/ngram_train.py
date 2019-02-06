b1041c228143a238cb48042ceeba6033dfb180c4
Pujaa Rajan <pujaarajan@Pujaas-MacBook-Pro.local>
2019-02-05 21:24:21 -0800

    updated ngram pkl file name

--- nlp_fib/src/ngram_train.py
+++ nlp_fib/src/ngram_train.py
@@ -23,17 +23,17 @@ class NgramTrain:
         bigram_counter = Counter()
         self.bigrams, self.bigram_count = self.ngram_generator_and_counter(preprocessed_dataframe, 2, bigram_counter)
         self.log.info(f'Bigram count: {list(self.bigram_count)[:1]}')
-        data.write_pickle_file(self.bigram_count, input_file_path, 'bigram_count.pickle', True)
+        data.write_pickle_file(self.bigram_count, input_file_path, 'bigram_count.pkl', True)
 
         self.bigram_forward_probability = Counter()
         self.ngram_probability(self.unigram_count, self.bigram_count, self.bigram_forward_probability, 'forward')
         self.log.info(f'Forward bigram probability example: {self.bigram_forward_probability.most_common(1)}')
-        data.write_pickle_file(self.bigram_forward_probability, input_file_path, 'bigram_forward_prob.pickle', True)
+        data.write_pickle_file(self.bigram_forward_probability, input_file_path, 'bigram_forward_probability.pkl', True)
 
         self.bigram_backward_probability = Counter()
         self.ngram_probability(self.unigram_count, self.bigram_count, self.bigram_backward_probability, 'backward')
         self.log.info(f'Backward bigram probability example: {self.bigram_backward_probability.most_common(1)}')
-        data.write_pickle_file(self.bigram_backward_probability, input_file_path, 'bigram_backward_prob.pickle', True)
+        data.write_pickle_file(self.bigram_backward_probability, input_file_path, 'bigram_backward_probability.pkl', True)
 
     def word_in_document_counter(self, preprocessed_dataframe):
         """This is creating a word count per document."""
