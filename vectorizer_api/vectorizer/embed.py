e75baf1a7fa027a422a64e59e8ea3b68dea54649
Ubuntu <ubuntu@ip-172-31-42-69.us-west-2.compute.internal>
2019-02-08 19:59:36 +0000

    failed to fix bug on aws

--- vectorizer_api/vectorizer/embed.py
+++ vectorizer_api/vectorizer/embed.py
@@ -7,15 +7,15 @@ sys.path.append(embedding_models_path)
 
 import word2vecReader
 
-TWITTER_WORD2VEC_EMBEDDING_PATH = "./vectorizer/word2vec_twitter_model/" + \
-	"word2vec_twitter_model.bin"
-TWITTER_WORD2VEC_EMBEDDING = word2vecReader.Word2Vec.\
-	load_word2vec_format(TWITTER_WORD2VEC_EMBEDDING_PATH,
-	binary=True)
+# TWITTER_WORD2VEC_EMBEDDING_PATH = "./vectorizer/word2vec_twitter_model/" + \
+# 	"word2vec_twitter_model.bin"
+# TWITTER_WORD2VEC_EMBEDDING = word2vecReader.Word2Vec.\
+#	load_word2vec_format(TWITTER_WORD2VEC_EMBEDDING_PATH,
+#	binary=True)
 # get vector dimension from sample word 'hello'
-HELLO_INDEX = TWITTER_WORD2VEC_EMBEDDING.vocab['hello'].index
-VECTOR_DIMENSION = len(TWITTER_WORD2VEC_EMBEDDING.syn0[HELLO_INDEX])
-# GLOVE_EMBEDDING = spacy.load('en_vectors_web_lg')
+# HELLO_INDEX = TWITTER_WORD2VEC_EMBEDDING.vocab['hello'].index
+# VECTOR_DIMENSION = len(TWITTER_WORD2VEC_EMBEDDING.syn0[HELLO_INDEX])
+GLOVE_EMBEDDING = spacy.load('en_vectors_web_lg')
 
 def glove_embedding(df, tokenizer):
 	embedding_matrix = np.zeros((vocab_size, 300))
@@ -31,13 +31,13 @@ def inference_glove_embedding(preprocessed_text):
 	embedding_matrix = np.zeros((len(preprocessed_text), VECTOR_DIMENSION))
 	for i in range(len(preprocessed_text)):
 		word = preprocessed_text[i]
-		# embedding_vector = GLOVE_EMBEDDING.vocab.get_vector(word)
-		try:
-			word_index = TWITTER_WORD2VEC_EMBEDDING.vocab[word].index
-			embedding_vector = TWITTER_WORD2VEC_EMBEDDING.syn0[word_index]
-		except KeyError:
-			print('word not in embedding')
-			embedding_vector = None
+		embedding_vector = GLOVE_EMBEDDING.vocab.get_vector(word)
+		# try:
+		#	word_index = TWITTER_WORD2VEC_EMBEDDING.vocab[word].index
+		#	embedding_vector = TWITTER_WORD2VEC_EMBEDDING.syn0[word_index]
+		# except KeyError:
+		#	print('word not in embedding')
+		#	embedding_vector = None
 		if embedding_vector is not None:
 			embedding_matrix[i] = embedding_vector
 
