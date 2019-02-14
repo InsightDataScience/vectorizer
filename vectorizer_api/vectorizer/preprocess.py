785dc045093d7c771cab1b594a8565c4fd3a5326
rexlintc <tc.lin@berkeley.edu>
2019-02-13 21:58:48 -0800

    fix list comprehension error

--- vectorizer_api/vectorizer/preprocess.py
+++ vectorizer_api/vectorizer/preprocess.py
@@ -2,7 +2,7 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords
 
-NLTK_STOPWORDS = stopwords.words('english')
+NLTK_STOPWORDS = set(stopwords.words('english'))
 
 def tokenize(text):
 	tokenizer = RegexpTokenizer(r'\w+')
@@ -10,7 +10,10 @@ def tokenize(text):
 	return tokenized_text
 
 def remove_stop_words(tokens):
-    no_stopwords = [token for token in tokens not in NLTK_STOPWORDS]
+    no_stopwords = []
+	for token in tokens:
+		if token not in NLTK_STOPWORDS:
+			no_stopwords.append(token)
     return no_stopwords
 
 def lemmatize_words(tokens):
